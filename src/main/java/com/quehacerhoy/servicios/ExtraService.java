2ce15ce222e1546bb71b90dc0d02c432ca3479fb
joseeg23 <joseeduardog23@gmail.com>
2020-11-17 19:40:45 -0300

    agrego metodos en el servicio y las clase de seguridad y de fechas

--- src/main/java/com/quehacerhoy/servicios/ExtraService.java
+++ src/main/java/com/quehacerhoy/servicios/ExtraService.java
@@ -5,9 +5,112 @@
  */
 package com.quehacerhoy.servicios;
 
+import com.quehacerhoy.entidades.Extra;
+import com.quehacerhoy.entidades.Foto;
+import com.quehacerhoy.entidades.Zona;
+import com.quehacerhoy.repositorios.ExtraRepositorio;
+import com.quehacerhoy.repositorios.ZonaRepositorio;
+import java.util.Date;
+import java.util.List;
+import javax.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ExtraService {
+    
+    @Autowired
+    private ExtraRepositorio repositorio;
+    @Autowired
+    private ZonaRepositorio zonaR;
+
+    //Alta para los eventos
+    @Transactional
+    public void altaEvento(String nombre, String descripcion, String direccion, String edad, String hora, String capacidad, Date fecha, Foto foto, String idZona) throws Exception {
+        
+        if (nombre.isEmpty()) {
+            throw new Exception("Debe indicar un nombre");
+        }
+        
+        if (descripcion.isEmpty()) {
+            throw new Exception("Debe indicar una descripcion");
+        }
+        if (edad.isEmpty()) {
+            throw new Exception("Debe indicar una edad referencial");
+        }
+        if (hora.isEmpty()) {
+            throw new Exception("Debe indicar la hora del evento");
+        }
+        
+        if (capacidad.isEmpty()) {
+            throw new Exception("Debe indicar la capacidad del evento");
+        }
+        
+        if (fecha == null) {
+            throw new Exception("Debe indicar una fecha valida");
+        }
+        if (foto == null) {
+            throw new Exception("Debe indicar una foto");
+        }
+        if (idZona.isEmpty()) {
+            throw new Exception("Debe indicar una zona");
+        }
+        try {
+            Extra evento = new Extra();
+            
+            evento.setCapacidad(capacidad);
+            evento.setDescripcion(descripcion);
+            evento.setDireccion(direccion);
+            evento.setEdad(edad);
+            evento.setFecha(fecha);
+            evento.setFoto(foto);
+            evento.setNombre(nombre);
+            //buscar zona con id
+            Zona zona = zonaR.getOne(idZona);
+            evento.setZona(zona);
+            
+            repositorio.save(evento);
+        } catch (Exception e) {
+            throw new Exception(e.getMessage());
+        }
+    }
+
+    //Alta para la publicidad
+    @Transactional
+    public void altaPublicidad(String nombre, String descripcion, Foto foto) throws Exception {
+        
+        if (nombre.isEmpty()) {
+            throw new Exception("Debe indicar un nombre");
+        }
+        
+        if (descripcion.isEmpty()) {
+            throw new Exception("Debe indicar una descripcion");
+        }
+        
+        if (foto == null) {
+            throw new Exception("Debe indicar una foto");
+        }
+        
+        try {
+            Extra publicidad = new Extra();
+            
+            publicidad.setDescripcion(descripcion);
+            
+            publicidad.setFoto(foto);
+            publicidad.setNombre(nombre);
+            Date fechaPublicacion = new Date();
+            publicidad.setFecha(fechaPublicacion);
+            
+            repositorio.save(publicidad);
+        } catch (Exception e) {
+            throw new Exception(e.getMessage());
+        }
+    }
+    
+     //lista de eventos
+    public List lista(){
+       return repositorio.listar(); 
+    }
+    
     
 }
--- src/main/java/com/quehacerhoy/servicios/UsuarioService.java
+++ src/main/java/com/quehacerhoy/servicios/UsuarioService.java
@@ -5,9 +5,129 @@
  */
 package com.quehacerhoy.servicios;
 
+import com.quehacerhoy.entidades.Usuario;
+import com.quehacerhoy.repositorios.UsuarioRepositorio;
+import java.util.ArrayList;
+import java.util.List;
+import javax.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 @Service
-public class UsuarioService {
+public class UsuarioService implements UserDetailsService  {
+    
+    @Autowired
+    private UsuarioRepositorio repositorio;
+    
+    //alta usuario admin que es el comercio que hara su publicidad
+    public void altaAdmin(String username, String nombre, String apellido, String email, String clave, String rol) throws Exception{
+         if (username.isEmpty()) {
+            throw new Exception("Debe indicar un username");
+        }
+          if (nombre.isEmpty()) {
+            throw new Exception("Debe indicar su nombre");
+        }
+           if (apellido.isEmpty()) {
+            throw new Exception("Debe indicar su apellido");
+        }
+            if (email.isEmpty()) {
+            throw new Exception("Debe indicar un email valido");
+        }
+             if (clave.isEmpty() || clave.length()<5) {
+            throw new Exception("La contraseña no debe estar vacio y debe tener al menos 5 caracteres");
+        }
+      try{       
+             Usuario admin = new Usuario();
+             admin.setUsername(username);
+             admin.setNombre(nombre);
+             admin.setApellido(apellido);
+             admin.setEmail(email);
+             admin.setRol("ADMIN");
+             String claveEncriptada = new BCryptPasswordEncoder().encode(clave);
+             admin.setClave(claveEncriptada);
+             
+             repositorio.save(admin);
+      }catch(Exception e){
+             throw new Exception(e.getMessage());
+      }   
+    }
+    
+    //alta usuario superadmin que seremos nosotros
+    public void altaSuperadmin(String username, String nombre, String apellido, String email, String clave, String rol) throws Exception{
+         if (username.isEmpty()) {
+            throw new Exception("Debe indicar un username");
+        }
+          if (nombre.isEmpty()) {
+            throw new Exception("Debe indicar su nombre");
+        }
+           if (apellido.isEmpty()) {
+            throw new Exception("Debe indicar su apellido");
+        }
+            if (email.isEmpty()) {
+            throw new Exception("Debe indicar un email valido");
+        }
+             if (clave.isEmpty() || clave.length()<5) {
+            throw new Exception("La contraseña no debe estar vacio y debe tener al menos 5 caracteres");
+        }
+      try{       
+             Usuario admin = new Usuario();
+             admin.setUsername(username);
+             admin.setNombre(nombre);
+             admin.setApellido(apellido);
+             admin.setEmail(email);
+             admin.setRol("SUPERADMIN");
+             String claveEncriptada = new BCryptPasswordEncoder().encode(clave);
+             admin.setClave(claveEncriptada);
+             
+             repositorio.save(admin);
+      }catch(Exception e){
+             throw new Exception(e.getMessage());
+      }   
+    }
+    
+     //lista de usuarios admin
+    public List lista(){
+       return repositorio.listarAdmin(); 
+    }
+    
+      @Override
+    public UserDetails loadUserByUsername(String username) {
+
+        try {
+            Usuario usuario = repositorio.getOne(username);
+
+            System.out.println(usuario.toString());
+            if (usuario != null) {
+                List<GrantedAuthority> permisos = new ArrayList();
+
+                GrantedAuthority p1 = new SimpleGrantedAuthority("ROLE_" + usuario.getRol());
+                permisos.add(p1);
+
+                ServletRequestAttributes attr = (ServletRequestAttributes) RequestContextHolder.currentRequestAttributes();
+                HttpSession session = attr.getRequest().getSession(true);
+                session.setAttribute("usuariosession", usuario);
+                User user = new User(usuario.getUsername(), usuario.getClave(), permisos);
+
+                return user;
+            } else {
+                return null;
+
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+            return null;
+        }
+    }
+    
+    
+    
     
 }
