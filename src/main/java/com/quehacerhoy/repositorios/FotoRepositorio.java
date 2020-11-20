5cfd93dd5db2a5500e6aeb7fe9ef62c821acb460
joseeg23 <joseeduardog23@gmail.com>
2020-11-20 19:48:35 -0300

    arreglo de pom.xml

--- src/main/java/com/quehacerhoy/repositorios/FotoRepositorio.java
+++ src/main/java/com/quehacerhoy/repositorios/FotoRepositorio.java
@@ -1,9 +1,6 @@
 
 package com.quehacerhoy.repositorios;
 import com.quehacerhoy.entidades.Foto;
-import com.quehacerhoy.entidades.Usuario;
-import java.util.ArrayList;
-import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
