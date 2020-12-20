456bbb408ecdd054a34dbf7f277f7b78a1e60002
joseeg23 <joseeduardog23@gmail.com>
2020-12-20 18:26:48 -0300

    hosteo

--- src/main/java/com/quehacerhoy/utilidades/ExceptionService.java
+++ src/main/java/com/quehacerhoy/utilidades/ExceptionService.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.quehacerhoy.excepciones;
+package com.quehacerhoy.utilidades;
 
 
 public class ExceptionService extends Exception{
